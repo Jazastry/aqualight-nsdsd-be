29b16ce6159c3078ec05ea08d28ed11f2478d580
Jazastry <jazastry@gmail.com>
2020-01-02 10:24:37 +0200

    adds seervice advertisment loop

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 var ip = require("ip");
-console.dir ( ip.address() );
 // import the module
 const mdns = require('mdns');
 
 var txt_record = {
   ip: ip.address().toString()
 };
-const ad = mdns.createAdvertisement(mdns.tcp('http'), 1337, {
-  name: 'aqualight',
-  networkInterface: ip.address(),
-  txtRecord: txt_record
-}, (err, data) => {
-  if (err) consle.error(err)
-  // console.log('data', data)
-});
+const getAd = () => {
+  return mdns.createAdvertisement(mdns.tcp('http'), 1337, {
+    name: 'aqualight',
+    networkInterface: ip.address(),
+    txtRecord: txt_record
+  }, (err, data) => {
+    if (err) consle.error(err)
+    // console.log('data', data)
+  });
+}
+let ad = getAd()
 ad.start();
+setInterval(() => {
+  ad.stop();
+  ad = getAd();
+  ad.start();
+}, 3000)
 
-var bonjour = require('bonjour')()
+// var bonjour = require('bonjour')()
 
 // // // advertise an HTTP server on port 3000
 // bonjour.publish({name: 'aqua', type: 'http', protocol: 'tcp', port: 1336, host: 'local'})
 // bonjour.publish({name: 'aqualight-b', type: 'http', protocol: 'tcp', port: 1337 })
 
 // // // // browse for all http services
-bonjour.find({
-  // type: 'http',
-  protocol: 'tcp',
-  // host: 'olinuxino.local',
-}, function (service) {
-  console.log('Found an HTTP server:', service)
-})
+// bonjour.find({
+//   type: 'http',
+//   protocol: 'tcp',
+//   // host: 'olinuxino.local',
+// }, function (service) {
+//   console.log('Found an HTTP server:', service)
+// })
 
